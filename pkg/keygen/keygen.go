a5e641323490fdba4a5c0ebadb760f9609788255
Amauri Bechtold Junior <amauribechtoldjr@gmail.com>
2024-07-15 09:54:20 -0300

    fix: remove unnused comments

--- pkg/keygen/keygen.go
+++ pkg/keygen/keygen.go
@@ -15,37 +15,3 @@ func ProcessKeyString(key string) string {
 		dateprocessor.New(time.Now()),
 		).Execute(key)
 	}
-
-const (
-	FULL_YEAR_CODE_SIGNAL     string = "{YY}"
-	DAYS_CODE_SIGNAL          string = "{dd}"
-	MONTHS_CODE_SIGNAL        string = "{mm}"
-	RANGE_NUMBERS_CODE_SIGNAL string = "{n:9}"
-)
-
-
-// func processFullYear(key *string) *string {
-// 	// fmt.Println(&key)
-// 	
-// }
-
-// func processMonths(key *string) *string {
-// 	// fmt.Println(&key)
-// 	*key = strings.ReplaceAll(*key, MONTHS_CODE_SIGNAL, time.Now().Format("01"))
-
-// 	return key
-// }
-
-// func processDays(key *string) *string {
-// 	// fmt.Println(&key)
-// 	*key = strings.ReplaceAll(*key, DAYS_CODE_SIGNAL, time.Now().Format("02"))
-
-// 	return key
-// }
-
-// func isValidSignal(key *string, signal string) bool {
-// 	// fmt.Println(&key)
-// 	signalIndex := strings.Index(*key, signal)
-
-// 	return signalIndex > -1
-// }
